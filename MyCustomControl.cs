80d19653d0b7a76e1f18764316d74b4fbefb7bfa
Pastajello <michal.sierpinski@pm.me>
2026-05-26 14:15:44 +0200

    add files

--- MyCustomControl.cs
+++ MyCustomControl.cs
@@ -1,6 +1,16 @@
 namespace CTM_BindableProperty_DynamicResource;
 
-public class MyCustomControl
+public partial class MyCustomControl : Grid
 {
+    [CommunityToolkit.Maui.BindableProperty]
+    public partial Color NotWorkingColor { get; set; }
     
+    public static readonly BindableProperty WorkingColorProperty =
+        BindableProperty.Create ("WorkingColor", typeof(Color), typeof(MyCustomControl), default(Color));
+    
+    public Color WorkingColor
+    {
+        get => (Color)GetValue(WorkingColorProperty);
+        set => SetValue(WorkingColorProperty, value);
+    }
 }
--- Platforms/iOS/AppDelegate.cs
+++ Platforms/iOS/AppDelegate.cs
@@ -5,5 +5,5 @@ namespace CTM_BindableProperty_DynamicResource;
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
